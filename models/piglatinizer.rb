1b25559cca049adeafcad52f59171fe9c0d6a156
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-18 17:23:44 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,7 @@ class PigLatinizer
   def piglatinize(phrase)
     #separate a string into individual words .split(" ")
     word_array = phrase.split(" ")
-    pig_phrase = word_array.map = 
+    pig_phrase = word_array.map {|word| piglatinize_word}
     
   end  
     
