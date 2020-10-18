d6cf28f157dce090d38ff28b19f6a5535e5a893a
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-18 17:37:35 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -15,7 +15,7 @@ class PigLatinizer
     if word.start_with?(/\b[aeiou]/) #starts with vowel
       word = "#{word}way" #add -way to end of word
     elsif word.start_with?(/\b[bcdfghjklmnpqrstvwxyz]+/)
-      parts = word.split(//)
+      parts = word.split(/([aeiou].*)/)
         #selects consonants from the beginning of the word
         #move them to the end of the word
         #add -ay
