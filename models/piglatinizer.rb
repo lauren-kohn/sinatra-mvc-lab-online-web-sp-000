4c024585cd8f92b402fc07096294b246c4437664
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-18 16:53:48 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,7 +10,8 @@ class PigLatinizer
     word_array.collect do |word|
       if word.start_with?(/\b[aeiou]/) #starts with vowel
         word = "#{word}ay" #add -way to end of word
-      #if /\b[bcdfghjklmnpqrstvwxyz]+/
+      elsif word.start_with?(/\b[bcdfghjklmnpqrstvwxyz]+/)
+        
         #selects consonants from the beginning of the word
         #move them to the end of the word
         #add -ay
