0a2bcc47cb3073f35df98a32263bd6e527f939ed
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-18 17:22:36 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,6 +7,10 @@ class PigLatinizer
   def piglatinize(phrase)
     #separate a string into individual words .split(" ")
     word_array = phrase.split(" ")
+    
+  end  
+    
+    
     word_array.collect do |word|
       if word.start_with?(/\b[aeiou]/) #starts with vowel
         word = "#{word}ay" #add -way to end of word
