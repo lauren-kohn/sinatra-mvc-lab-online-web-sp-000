2efb823f7e961da638cfa8b8880a8a4ca7447c8c
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-18 16:45:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,6 +9,9 @@ class PigLatinizer
     if /\b[aeiou]/ #starts with vowel
       #add -way to end of word
     if /\b[bcdfghjklmnpqrstvwxyz]+/
+      #selects consonants from the beginning of the word
+      #move them to the end of the word
+      #add -ay
   end
   
 end
