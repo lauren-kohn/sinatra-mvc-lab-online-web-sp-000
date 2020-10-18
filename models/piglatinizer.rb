fec899071da05df3df29da3eb0befd601fd0a78c
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-10-18 16:50:04 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,12 +6,17 @@ class PigLatinizer
   
   def piglatinize(phrase)
     #separate a string into individual words .split(" ")
-    #if /\b[aeiou]/ #starts with vowel
-      #add -way to end of word
-    #if /\b[bcdfghjklmnpqrstvwxyz]+/
-      #selects consonants from the beginning of the word
-      #move them to the end of the word
-      #add -ay
+    word_array = phrase.split(" ")
+    word_array.collect do |word|
+      if word./\b[aeiou]/ #starts with vowel
+        #add -way to end of word
+      #if /\b[bcdfghjklmnpqrstvwxyz]+/
+        #selects consonants from the beginning of the word
+        #move them to the end of the word
+        #add -ay
+      end 
+    end 
+    word_array
   end
   
 end
